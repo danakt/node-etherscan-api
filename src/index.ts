871f9d1b4af6be824dd625f7426aa6f1afe1ab29
Danakt Frost <danakt_frost@mail.ru>
2018-03-03 01:48:00 +0300

    Add get blaance functions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { NETWORKS } from './constants/networks'
+import { NETWORKS }      from './constants/networks'
+import { createRequest } from './utils/createRequest'
+import { MODULES }       from './constants/modules'
+import { ACTIONS }       from './constants/actions'
 
 /**
  * Etherscan API
@@ -10,8 +13,8 @@ export = class EtherscanApi {
   /** Network name */
   private network: keyof typeof NETWORKS
 
-  /** Network url */
-  private url: string
+  /** Network API host */
+  private host: string
 
   /**
    * @constructor
@@ -26,6 +29,50 @@ export = class EtherscanApi {
     this.network
       = netName in NETWORKS ? (netName as keyof typeof NETWORKS) : 'MAIN'
 
-    this.url = NETWORKS[this.network]
+    this.host = NETWORKS[this.network]
+  }
+
+  /**
+   * Returns Ether Balance for a single Address
+   * @param address
+   * @param {string?} [unit="wei"] Balance unit
+   * @return {Promise<string>}
+   */
+  public async getAccountBalance(
+    address: string,
+    unit: string = 'wei'
+  ): Promise<string> {
+    const resp = await createRequest(this.host, {
+      apikey: this.token,
+      action: ACTIONS.BALANCE,
+      module: MODULES.ACCOUNT,
+      tag:    'latest',
+      address
+    })
+
+    return resp
+  }
+
+  /**
+   * Get Ether Balance for multiple Addresses in a single call
+   * @param {Array<string>} addresses
+   * @param {string?} [unit="wei"] Balance unit
+   */
+  public async getAccountBalances(
+    addresses: string[],
+    unit: string = 'wei'
+  ): Promise<{ account: string; balance: string }> {
+    const resp: { account: string; balance: string } = await createRequest(
+      this.host,
+      {
+        apikey:  this.token,
+        action:  MODULES.ACCOUNT,
+        module:  ACTIONS.BALANCE_MULTI,
+        tag:     'latest',
+        address: addresses.join(',')
+      }
+    )
+
+    return resp
   }
 }
