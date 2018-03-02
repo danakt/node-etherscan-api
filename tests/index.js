e629b2a85e1bdd81c2040975a5b44a20cc253160
Danakt Frost <danakt_frost@mail.ru>
2018-03-03 01:24:58 +0300

    Add tes

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,15 @@
+const EtherscanAPI = require('../build')
+
 describe('Etherscan API', () => {
-  // test()
+  test('Instance', () => {
+    const etherscan1 = new EtherscanAPI()
+    expect(etherscan1.token).toBe('YourApiKeyToken')
+    expect(etherscan1.network).toBe('MAIN')
+    expect(etherscan1.host).toBe('https://api.etherscan.io')
+
+    const etherscan2 = new EtherscanAPI('123', 'ropsten')
+    expect(etherscan2.token).toBe('123')
+    expect(etherscan2.network).toBe('ROPSTEN')
+    expect(etherscan2.host).toBe('http://api-ropsten.etherscan.io')
+  })
 })
