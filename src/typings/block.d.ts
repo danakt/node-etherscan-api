7f609ccee4e62411c5264aa07f03df54d8c5a0b0
Danakt Frost <danakt_frost@mail.ru>
2018-03-04 00:18:48 +0300

    add geth block info

--- src/typings/block.d.ts
+++ src/typings/block.d.ts
@@ -14,3 +14,26 @@ declare interface BlockRewardInfo extends BlockInfo {
     unclePosition: string
   }>
 }
+
+declare interface GethBlockInfo {
+  difficulty: string
+  extraData: string
+  gasLimit: string
+  gasUsed: string
+  hash: string
+  logsBloom: string
+  miner: string
+  mixHash: string
+  nonce: string
+  number: string
+  parentHash: string
+  receiptsRoot: string
+  sha3Uncles: string
+  size: string
+  stateRoot: string
+  timestamp: string
+  totalDifficulty: string
+  transactions: TransactionDescription[]
+  transactionsRoot: string
+  uncles: any[]
+}
