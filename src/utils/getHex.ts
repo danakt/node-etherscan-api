2b464e74b150a1fd48ff164e143c7b6e79278c67
Danakt Frost <danakt_frost@mail.ru>
2018-03-04 02:28:33 +0300

    Update getHex

--- src/utils/getHex.ts
+++ src/utils/getHex.ts
@@ -1,8 +1,8 @@
 /**
  * Return hexadecimal number with "0x" from decimal number
- * @param {number} num Decimal number
+ * @param {number} [num=0] Decimal number
  * @return {number} Hexadecimal number
  */
-export function getHex(num: number): string {
+export function getHex(num: number = 0): string {
   return '0x' + num.toString(16)
 }
